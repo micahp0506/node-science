0c74c4dafb98eeaa2be8e2fdc3edf82f7309156f
Micah Wells <micahp0506@gmail.com>
2016-03-08 09:35:50 -0600

    add size limits to stack

---
+++ stack.js
@@ -0,0 +1,55 @@
+'use strict'
+
+class Node {
+  constructor (data) {
+    this.payload = data
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor (size) {
+    this.head = null
+    this.maxSize = size
+    this.length = 0
+  }
+
+  push (item) {
+    if (this.isFull()) {
+      throw new Error('Stack full')
+    }
+
+    let node
+
+    if (item instanceof Node) {
+      node = item
+    } else {
+      node = new Node(item)
+    }
+
+    node.next = this.head
+    this.head = node
+
+    this.length++
+  }
+
+  pop () {
+    const toRemove = this.head
+
+    this.head = toRemove.next
+    this.length--
+
+    return toRemove
+  }
+
+  isEmpty () {
+    return !this.length
+  }
+
+  isFull () {
+    return this.length >= this.maxSize
+  }
+
+  getSize () {
+    return this.length
+  }
