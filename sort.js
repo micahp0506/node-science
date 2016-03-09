26b4064e6b7b449632cd4aea0317fffa1b386e0e
Micah Wells <micahp0506@gmail.com>
2016-03-09 12:04:08 -0600

    insertion sort working

--- sort.js
+++ sort.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const chalk = require('chalk')
+const util = require('util')
 
 const arr = [6,5,3,1,8,7,2,4]
 
@@ -13,19 +14,33 @@ function swap (array, index1, index2) {
   array[index2] = temp
 }
 
-function bubbleSort (array) {
-  for (let j = 1; j < array.length; j++) {
-    for (let i = 0; i < array.length - j; i++) {
-      if (array[i] > array[i + 1]) {
-        swap(array, i, i + 1)
-        console.log(chalk.red(JSON.stringify(array)))
-      }
-      console.log(JSON.stringify(array))
-    }
-  }
+// function bubbleSort (array) {
+//   for (let j = 1; j < array.length; j++) {
+//     for (let i = 0; i < array.length - j; i++) {
+//       if (array[i] > array[i + 1]) {
+//         swap(array, i, i + 1)
+//         console.log(chalk.red(util.inspect(array)))
+//       }
+//       console.log(chalk.dim(util.inspect(array)))
+//     }
+//   }
 
-  return array
+//   return array
+// }
+// console.log('bubbled', bubbleSort(arr))
+
+function insertionSort (array) {
+    for (let i = 1; i< array.length; i++) {
+        for(let j = i; j > 0; j--) {
+            if (array[j - 1] > array[j]) {
+                swap(array, j-1, j);
+                console.log(chalk.red(util.inspect(array)));
+           } else {
+                console.log(chalk.dim(util.inspect(array)));
+           }
+        }
+    }
+    return array;
 }
 
-console.log('bubble', bubbleSort(arr))
-
+console.log('insertion', insertionSort(arr))
